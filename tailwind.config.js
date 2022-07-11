454b61cc2b89835d0bf6f250789fe549dde1debf
Anmol-KM <anmol@kareermatrix.com>
2022-07-11 21:31:51 +0530

    Add tailwind to dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,7 @@ module.exports = {
       
       'tab': { 'max': '900px', 'min': '550px' },
     },
-    extend: {
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-        adelia: ["ADELIA", "cursive"],
-      },
-      animation: {
-        bounce200: 'bounce 1s infinite 200ms',
-        bounce400: 'bounce 1s infinite 400ms',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
